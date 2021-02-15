2e0afa51f9c9b75743fb1094517513d1695cf375
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-15 23:09:49 +0900

    first commit

---
+++ baekjoon/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "solution.h"
+
+using namespace std;
+
+int main()
+{
+    solution_2839();
+    //cout << "Hello! I'm seongwon" << endl;
+    return 0;
+}
+
+// command + shift + B ==> g++ build (generate *.out)
+// command + shift + D ==> Debug mode
---
+++ baekjoon/solution_2839.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "solution.h"
+
+using namespace std;
+
+void solution_2839() {
+    cout << "Hello_solution_2839" << endl;
+}
