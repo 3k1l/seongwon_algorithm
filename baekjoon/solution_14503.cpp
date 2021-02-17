cf452c7a22a042fda1071c2273b02d4a9ea841a1
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-17 23:50:12 +0900

    2021_0217_solving_problem

--- baekjoon/solution_14503.cpp
+++ baekjoon/solution_14503.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int n, m;
+int x, y, dir;
 
-  return 0;
+        //   N  E  S  W
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+int position[50][50];
+int map[50][50];
+
+void turn_left() {
+    dir -= -1;
+    if(dir == -1) {
+        dir = 3;
+    }
+}
+
+int main(void) {
+    cin >> n >> m;
+    cin >> x >> y >> dir;
+    position[x][y] = 1;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    return 0;
 }
