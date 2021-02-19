b8ca04bdcf76d7a0cfe9971f738c82052b23d954
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-19 23:42:38 +0900

    2021_0219

--- baekjoon/solution_14503.cpp
+++ baekjoon/solution_14503.cpp
@@ -28,12 +28,5 @@ int main(void) {
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << map[i][j];
-        }
-        cout << endl;
-    }
-
     return 0;
 }
