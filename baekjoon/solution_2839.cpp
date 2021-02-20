004e7bf1783232660e8bc90f9632b034b49e5e68
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-20 20:12:08 +0900

    2021_0220_SECER_Success

--- baekjoon/solution_2839.cpp
+++ baekjoon/solution_2839.cpp
@@ -12,26 +12,28 @@ int main() {
   cin >> n;
   m = n / cointypes[0];  // maximum value of 5kg
   while(m >= 0) {
-    cout << "m : " << m << endl;
+    sugar = 0;
+    //cout << "m : " << m << endl;
     if (m > 0) {
       r = n - m * cointypes[0];
-      cout << "r : " << r << endl;
+      //cout << "r : " << r << endl;
       sugar = m;
-      cout << "sugar : " << sugar << endl;
+      //cout << "sugar : " << sugar << endl;
     }
     else {
       r = n;
-      cout << "r : " << r << endl;
+      //cout << "r : " << r << endl;
     }
 
     if (r % cointypes[1] == 0) {
       sugar += r/cointypes[1];
-      cout << "sugar : " << sugar << endl;
+      //cout << "sugar : " << sugar << endl;
       break;
     }
     m--;
+    if(m < 0) {
+      sugar = -1;
+    }
   }
   cout << sugar << endl;
-  //0%5 = 0;
-  //18%0 = error;
 }
