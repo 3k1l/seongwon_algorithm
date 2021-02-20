ee99a70d3e201bcafc4fe56efe4a766e51c843fd
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-20 21:11:15 +0900

    2021_0220_solving...

--- baekjoon/solution_14503.cpp
+++ baekjoon/solution_14503.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#define MAX 50
+
 using namespace std;
 
 int n, m;
 int x, y, dir;
+int cnt;
 
         //   N  E  S  W
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
-int position[50][50];
-int map[50][50];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int map[MAX][MAX];
+bool check[MAX][MAX] = {false};;
 
 void turn_left() {
     dir -= -1;
@@ -17,10 +20,14 @@ void turn_left() {
     }
 }
 
+void go_back() {
+    
+}
+
 int main(void) {
     cin >> n >> m;
     cin >> x >> y >> dir;
-    position[x][y] = 1;
+    map[x][y] = 1;
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
@@ -28,11 +35,19 @@ int main(void) {
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << map[i][j];
+    while(true) {
+        if(!check[x][y]) cnt++;
+        check[x][y] = true;
+
+        bool clean = true;
+
+        for (int i = 1; i <= 4; i++) {
+            int nd = (4 + dir - i) % 4;
+            int nx = x + dx[nd];
+            int ny = y + dy[nd];
+
         }
-        cout << endl;
+
     }
 
     return 0;
