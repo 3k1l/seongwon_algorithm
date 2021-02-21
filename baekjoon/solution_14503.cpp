0adea8573bd8e53454f1f98ddb4ac80b09fd702b
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-21 16:05:32 +0900

    2021_0221_solve_robotic_vacuum

--- baekjoon/solution_14503.cpp
+++ baekjoon/solution_14503.cpp
@@ -11,18 +11,7 @@ int cnt;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 int map[MAX][MAX];
-bool check[MAX][MAX] = {false};;
-
-void turn_left() {
-    dir -= -1;
-    if(dir == -1) {
-        dir = 3;
-    }
-}
-
-void go_back() {
-    
-}
+bool check[MAX][MAX] = {false};
 
 int main(void) {
     cin >> n >> m;
@@ -36,19 +25,42 @@ int main(void) {
     }
 
     while(true) {
-        if(!check[x][y]) cnt++;
-        check[x][y] = true;
+        if(false == check[x][y]) {
+            cnt++;                  // 1. clean
+        }
+        check[x][y] = true;         // true : clean / false : not yet
 
-        bool clean = true;
+        bool fclean = true;
 
+        // 2. explorer 4 direction
         for (int i = 1; i <= 4; i++) {
             int nd = (4 + dir - i) % 4;
             int nx = x + dx[nd];
             int ny = y + dy[nd];
 
+            if (true == check[nx][ny]) continue;    // 2_b : already cleaned
+            if (1 == map[nx][ny]) continue; // 2_b : wall
+            // 2_a : turn left and go 1 block
+            fclean = false;
+            x = nx;
+            y = ny;
+            dir = nd;
+            break;
+        }
+
+        if (true == fclean) {
+            // 2_c
+            int nx = x + dx[(2 + dir) % 4];
+            int ny = y + dy[(2 + dir) % 4];
+            // 2_d : finish
+            if(map[nx][ny] == 1) break;
+            else {
+                x = nx;
+                y = ny;
+            }
         }
 
     }
-
+    cout << cnt;
     return 0;
 }
