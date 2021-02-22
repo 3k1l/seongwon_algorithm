1f656806503663836b772f44dd266999f583b90a
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-22 23:52:17 +0900

    2021_0222_study_ch1

---
+++ cpp_study/CH2_Beginning_cpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>                             // include header file
+
+using namespace std;
+int main()
+{
+    cout << "C++ is better than C." << endl;    // C++ statement
+
+    return 0;                                   // End of example
+}
