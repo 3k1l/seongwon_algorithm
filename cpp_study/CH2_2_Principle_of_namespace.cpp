4e41e8ab293b8bb64997a4e18b81c980ae6b3a67
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-23 23:58:44 +0900

    2021_0223_study_cpp

---
+++ cpp_study/CH2_2_Principle_of_namespace.cpp
@@ -0,0 +1,14 @@
+/*
+    namespace 키워드
+    - 이름 충돌 해결 : 개발자가 자신만의 이름 공간을 생성할 수 있도록 함
+
+    std:: 생략
+    - using 지시어 사용
+
+    using std::cout;                // cout에 대해서만 std:: 생략
+    cout << "Hello" << std::endl;   // std::cout에서 std:: 생략
+
+    using namespace std;            // std 이름 공간에 선언된 모든 이름의 std:: 생략
+    cout << "Hello" << endl;        // std:: 생략
+
+*/
