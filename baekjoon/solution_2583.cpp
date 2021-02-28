11896ef722544925d3416206779c1df5cc227dd5
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-28 19:48:56 +0900

    2021_0228_Area_Calculator

--- baekjoon/solution_2583.cpp
+++ baekjoon/solution_2583.cpp
@@ -5,26 +5,72 @@
 
 using namespace std;
 
-int n, m, k;
-int x1, x2, y1, y2;
+int M,N,k;
+int cnt;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
 int map[MAX][MAX];
-int rectangle[MAX][MAX];
+bool visit[MAX][MAX];
+vector<int> result;
+
+void dfs(int x, int y) {
+    visit[x][y] = true;
+    cnt ++;
+
+    for(int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(0 <= nx && nx < M && 0 <= ny && ny < N){
+            if((map[nx][ny] == 0) && !visit[nx][ny]) {
+                dfs(nx,ny);
+            }
+        }
+
+    }
+}
 
 int main() {
-    cin >> n >> m >> k;
+    cin >> M >> N >> k;
+
+    int x1, x2, y1, y2;
 
     for (int i = 0; i < k; i++) {
-        for (int j = 0; j < 4; j++) {
-            cin >> rectangle[i][j];
-        }
-        x1 = rectangle[i][0];
-        y1 = rectangle[i][1];
-        x2 = rectangle[i][2];
-        y2 = rectangle[i][3];
+        cin >> y1 >> x1 >> y2 >> x2;
 
-        
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                map[x][y] = 1;
+            }
+        }
     }
 
+    cout << endl;
 
+    for (int x = 0; x < M; x++) {
+        for (int y = 0; y < N; y++) {
+            cout << map[x][y] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int x = 0; x < M; x++) {
+        for (int y = 0; y < N; y++) {
+            cnt = 0;
+            dfs(x,y);
+            result.push_back(cnt);
+        }
+    }
+
+    cout << result.size() << endl;
+
+    sort(result.begin(), result.end());
+
+    for(int i = 0; i < k; i++) {
+        cout << result[i] << ' ';
+    }
     
+    return 0;
 }
