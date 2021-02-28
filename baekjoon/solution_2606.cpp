378b05df72767c468498785959ff767f265b99d6
Seongwon Kim <seongwon@Seongwonui-MacBookPro.local>
2021-02-28 23:26:44 +0900

    2021_0228_DFS_Virus

---
+++ baekjoon/solution_2606.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#define MAX 100
+
+using namespace std;
+
+int n;
+int node, neighbor_node;
+int cnt;
+bool visited[MAX];
+vector<int> graph[MAX];
+
+void dfs(int x) {
+    visited[x] = true;
+    cnt++;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < graph[i].size(); j++) {
+            if(!visited[i]) {
+                dfs(i);
+            }
+        }
+
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> node >> neighbor_node;
+        graph[node].push_back(neighbor_node);
+    }
+
+    dfs(1);
+    cout << cnt << endl;
+}
