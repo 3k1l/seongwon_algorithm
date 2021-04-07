1a474616c1828d152546f27a15ffc430a0973115
newmoon93 <seongwon93@gmail.com>
2021-04-08 01:18:29 +0900

    2021_0408_fix_Dynamic

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 |9|[1946](https://www.acmicpc.net/problem/1946)|Greedy|신입사원|Success|2021-03-07|pair클래스 정렬|
 |10|[1463](https://www.acmicpc.net/problem/1463)|Dynamic|1로만들기|Success|2021-03-14||
 |11|[11726](https://www.acmicpc.net/problem/11726)|Dynamic|2xN타일링|Success|2021-04-06|연산 순서 주의|
-|12|[9095](https://www.acmicpc.net/problem/9095)|Dynamic|1,2,3더하기|fail|2021-03-14|맞왜틀|
+|12|[9095](https://www.acmicpc.net/problem/9095)|Dynamic|1,2,3더하기|Success|2021-04-08|입출력다시확인|
 |13|[2178](https://www.acmicpc.net/problem/2178)|BFS|미로탐색|Success|2021-03-17||
 |14|[2667](https://www.acmicpc.net/problem/2667)|BFS|단지번호붙이기|fail|0000-00-00||
 |15|[7576](https://www.acmicpc.net/problem/7576)|BFS|토마토|fail|0000-00-00||
--- baekjoon/solution_9095.cpp
+++ baekjoon/solution_9095.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int n;
 int t;
-int dp[10];
+int dp[11];
+vector<int> v;
 
 int main()
 {
@@ -16,16 +18,32 @@ int main()
     for(int i = 0; i < t; i++)
     {
         cin >> n;
-        if(n <= 3)
-            cout << dp[n] << endl;
+        v.push_back(n);
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        if(v[i] <= 3)
+            cout << dp[v[i]] << endl;
         else
         {
-            for (int j = 4; j <= n; j++)
+            for (int j = 4; j <= v[i]; j++)
             {
                 dp[j] = dp[j-3] + dp[j-2] + dp[j-1];
             }
-            cout << dp[n] << endl;
+            cout << dp[v[i]] << endl;
         }
     }
-
 }
+/*
+1   1
+2   2
+3   4
+4   7
+5   13
+6   24
+7   44
+8   81
+9   149
+10  274
+*/
