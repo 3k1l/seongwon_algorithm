9c8215c69d92c3166ba942c9a411a7ae07ad990f
newmoon93 <seongwon93@gmail.com>
2021-04-18 23:09:15 +0900

    2021_0418_dynamic

--- baekjoon/solution_1932.cpp
+++ baekjoon/solution_1932.cpp
@@ -4,45 +4,74 @@
 using namespace std;
 
 int n;
-int x;
 int sum;
-int dy[2] = {1, 1};
-int dx[2] = {0, 1};
-bool visit[500][500];
-vector<int> tri[500];
-
-void dfs(int x, int y)
-{
-    visit[x][y] = true;
-    sum += tri[x][y];
-
-    for(int i = 0; i < 2; i++)
-    {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-
-        if((nx < n) & (ny < n))
-        {
-            cout << "sum : " << sum;
-            cout << "nx : " << nx << " ny : " << ny << endl;
-            dfs(nx,ny);
-        }
-    }
-}
+int triangle[501][501];
+// int x;
+// int dy[2] = {1, 1};
+// int dx[2] = {0, 1};
+// vector<int> tri[500];
 
 int main()
 {
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cin >> x;
-            tri[i].push_back(x);
+            cin >> triangle[i][j];
         }
     }
-    dfs(0,0);
-    cout << "final sum : " << sum << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j]);
+        }
+    }
 
-    return 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (sum < triangle[n][i])
+        {
+            sum = triangle[n][i];
+        }
+    }
+    cout << sum << endl;
 }
+
+/*** dfs method ***/
+// void dfs(int x, int y)
+// {
+//     visit[x][y] = true;
+//     sum += tri[x][y];
+
+//     for(int i = 0; i < 2; i++)
+//     {
+//         int nx = x + dx[i];
+//         int ny = y + dy[i];
+
+//         if((nx < n) & (ny < n))
+//         {
+//             cout << "sum : " << sum;
+//             cout << "nx : " << nx << " ny : " << ny << endl;
+//             dfs(nx,ny);
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             cin >> x;
+//             tri[i].push_back(x);
+//         }
+//     }
+//     dfs(0,0);
+//     cout << "final sum : " << sum << endl;
+
+//     return 0;
+// }
